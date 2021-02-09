ebb8d74f114552a64fe3914bbded149e85cb41a4
Sarit Maitra <sarit.maitra@gmail.com>
2021-02-09 16:32:30 +0800

    Update ADFInstance_Creation.ps1

--- ADFInstance_Creation.ps1
+++ ADFInstance_Creation.ps1
@@ -1,19 +1,19 @@
 cd .\.azure # .azure directory in local machine
 
 # If not installed already
-Install-Module -Name AzureRM
+Install-Module -Name AzRM
 
 # update existing Azure PowerShell module
-Update-Module -Name AzureRM
+Update-Module -Name AzRM
 
-Connect-AzureRmAccount
+Connect-AzRmAccount
 
-Get-AzureRmSubscription
+Get-AzRmSubscription
 
 # In case of multiple subscriptions associated with account, run the following command to select the subscription that you want to work with.
 # Replace SubscriptionId with the ID of my Azure subscription:
-Select-AzureRmSubscription -SubscriptionId "<SubscriptionId>"
+Select-AzRmSubscription -SubscriptionId "<SubscriptionId>"
 
 # Create a Data Factory
 
-Set-AzureRmDataFactoryV2 -ResourceGroupName rgname -Location eastus -Name adfdemo
+Set-AzRmDataFactoryV2 -ResourceGroupName rgname -Location eastus -Name adfdemo
